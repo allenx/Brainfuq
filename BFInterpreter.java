3c5d74d37234cd970ca34a83d3bedafe33f07a4e
allenx <mint.mr@gmail.com>
2017-05-09 19:07:18 +0800

    Fixed a bug in the REPL Environment

--- BFInterpreter.java
+++ BFInterpreter.java
@@ -12,21 +12,17 @@ import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
 
-public class TreeVisitor extends BrainfuqBaseVisitor {
+public class BFInterpreter extends BrainfuqBaseVisitor {
 
-    private static Logger logger = Logger.getLogger(TreeVisitor.class.getName());
+    private static Logger logger = Logger.getLogger(BFInterpreter.class.getName());
 
     private int LENGTH = 65443;
-    protected byte[] tape = new byte[LENGTH];
-    protected int dataPointer = 0;
+    byte[] tape = new byte[LENGTH];
+    int dataPointer = 0;
 
     //For the input
     private Scanner sc = new Scanner(System.in);
 
-//    public TreeVisitor() {
-////        System.out.print(LENGTH);
-////        System.out.print((char) tape[0]);
-//    }
 
     @Override
     public Object visitTop(BrainfuqParser.TopContext ctx) {
@@ -79,7 +75,6 @@ public class TreeVisitor extends BrainfuqBaseVisitor {
 //        System.out.print("Enterted Loop\n");
         while (tape[dataPointer] != 0) {
             visitChildren(ctx);
-//            tape[dataPointer] = 0;
         }
 
 
@@ -89,21 +84,46 @@ public class TreeVisitor extends BrainfuqBaseVisitor {
 
 
     public static void main(String[] args) throws IOException {
-        TreeVisitor treeVisitor = new TreeVisitor();
-        try {
+        BFInterpreter bfInt = new BFInterpreter();
+
+        if (args[0].equals("-i")) {
+            Scanner commands = new Scanner(System.in);
+            System.out.print("Welcome to Brainfuck Interactive REPL :)\n");
+
+            while (true) {
+                System.out.print("BF >> ");
+                String command = commands.next();
+                System.out.print("\n    ");
+                final ANTLRInputStream input = new ANTLRInputStream(command);
+                BrainfuqLexer lexer = new BrainfuqLexer(input);
+                CommonTokenStream tokens = new CommonTokenStream(lexer);
+                BrainfuqParser parser = new BrainfuqParser(tokens);
+                bfInt.visit(parser.top());
+                System.out.print("\n\n");
+
+                bfInt.dataPointer = 0;
+                bfInt.tape = null;
+
+            }
+                
+        } else {
+            try {
             final FileInputStream inStream = new FileInputStream(args[0]);
             final ANTLRInputStream input = new ANTLRInputStream(inStream);
             BrainfuqLexer lexer = new BrainfuqLexer(input);
             CommonTokenStream tokens = new CommonTokenStream(lexer);
             BrainfuqParser parser = new BrainfuqParser(tokens);
-            treeVisitor.visit(parser.top());
+            bfInt.visit(parser.top());
 
 //            logger.log(Level.INFO, "p: {0}; tape: {1}", new Object[] {
-//                    treeVisitor.p,
-//                    Arrays.toString(treeVisitor.tape)
+//                    bfInt.p,
+//                    Arrays.toString(bfInt.tape)
 //            });
-        } catch (IOException foo) {
-            foo.printStackTrace();
+            } catch (IOException foo) {
+                foo.printStackTrace();
+            }
+
         }
+
     }
 }
